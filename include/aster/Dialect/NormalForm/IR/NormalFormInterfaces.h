f6995b11399885d8f088f136a7eef39c13fcf790
Oleksandr "Alex" Zinenko <azinenko@amd.com>
2026-03-09 12:57:59 +0100

    Dialect for normal forms (#319)
    This introduces a dialect for normal forms. It defines an operation to
    which additional attribute/type/op verification hooks can be attached
    based on an attribute interface implementation. This is useful to encode
    expectations that passes have from the IR without defining a full new
    dialect.
    
    ---------
    
    Signed-off-by: Alex Zinenko <git@ozinenko.com>
    Signed-off-by: Nicolas Vasilache <nicolasvasilache@users.noreply.github.com>
    Co-authored-by: Tim Gymnich <tim.gymnich@amd.com>
    Co-authored-by: Martin Luecke <martin.luecke@amd.com>
    Co-authored-by: Nicolas Vasilache <nicolasvasilache@users.noreply.github.com>

---
+++ include/aster/Dialect/NormalForm/IR/NormalFormInterfaces.h
@@ -0,0 +1,43 @@
+//===- NormalFormInterfaces.h ---------------------------------------------===//
+//
+// Copyright 2026 The ASTER Authors
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef ASTER_DIALECT_NORMALFORM_IR_NORMALFORMINTERFACES_H
+#define ASTER_DIALECT_NORMALFORM_IR_NORMALFORMINTERFACES_H
+
+#include "mlir/IR/Attributes.h"
+#include "mlir/IR/OpDefinition.h"
+#include "mlir/Support/LLVM.h"
+
+#include "aster/Dialect/NormalForm/IR/NormalFormAttrInterfaces.h.inc"
+
+namespace normalform {
+
+/// Verify that all IR nested under `root` satisfies the given normal form.
+/// If `emitDiagnostics` is true, errors are reported; otherwise the check
+/// is silent (useful for inferNormalForms).
+::llvm::LogicalResult verifyNormalForm(::mlir::Operation *root,
+                                       NormalFormAttrInterface normalForm,
+                                       bool emitDiagnostics);
+
+} // namespace normalform
+
+namespace llvm {
+template <>
+struct PointerLikeTypeTraits<normalform::NormalFormAttrInterface>
+    : public PointerLikeTypeTraits<mlir::Attribute> {
+  static inline normalform::NormalFormAttrInterface
+  getFromVoidPointer(void *p) {
+    return normalform::NormalFormAttrInterface(
+        mlir::Attribute::getFromOpaquePointer(p));
+  }
+};
+} // namespace llvm
+
+#endif // ASTER_DIALECT_NORMALFORM_IR_NORMALFORMINTERFACES_H
